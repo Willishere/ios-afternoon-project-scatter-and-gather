dc92edc6c4d3f8b672dd91eb6d840d9cf2407b62
William Chen <chen.will7@gmail.com>
2019-09-10 12:27:20 -0700

    fade out working

--- Gather and Scatter/Gather and Scatter/ViewController.swift
+++ Gather and Scatter/Gather and Scatter/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var LLabel: UILabel!
+    @IBOutlet weak var ALabel: UILabel!
+    @IBOutlet weak var MLabel: UILabel!
+    @IBOutlet weak var BLabel: UILabel!
+    @IBOutlet weak var DLabel: UILabel!
+    @IBOutlet weak var AendLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,6 +27,9 @@ class ViewController: UIViewController {
         var isScattered: Bool = true
         isScattered != isScattered
         if isScattered{
+            UIView.animate(withDuration: 1.5) {
+                self.imageView.alpha = 0
+            }
             
         }else {
             
